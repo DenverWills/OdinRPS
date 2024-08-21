07e7d5c98c34cbf8fd9d699ac3fcb91afeccc64f
DenverWills <156851531+DenverWills@users.noreply.github.com>
2024-08-21 14:58:19 -0500

    Added getComputerChoice

--- RPS.js
+++ RPS.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+function getComputerChoice(){
+    const randoNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+
+    if(randoNumber === 0){
+        computerChoice = "Rock";
+    }
+    else if(randoNumber === 1){
+        computerChoice = "Paper";
+    }
+    else{
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
